d6eed456693dac3485cea37dbb2411500894b6ab
Your Name <parker.josie.eden@gmail.com>
2023-03-13 09:23:42 -0500

    got all the tables set up

--- db/seedData.js
+++ db/seedData.js
@@ -10,6 +10,42 @@ async function dropTables() {
 async function createTables() {
   console.log("Starting to build tables...")
   // create all tables, in the correct order
+  console.log("Creating users");
+await client.query(`
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) UNIQUE NOT NULL
+);`);
+
+await client.query(`
+CREATE TABLE activities(
+  id SERIAL PRIMARY KEY,
+  name  VARCHAR(255) UNIQUE NOT NULL,
+  description TEXT NOT NULL
+);
+`)
+
+await client.query(`
+CREATE TABLE routines(
+  id SERIAL PRIMARY KEY,
+  "creatorId" INTEGER REFERENCES users(id),
+  "isPublic" BOOLEAN DEFAULT false,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  goal TEXT NOT NULL
+);
+`)
+
+await client.query(`
+CREATE TABLE routine_activities(
+  id SERIAL PRIMARY KEY,
+  "routineId" INTEGER REFERENCES routines(id),
+  "activityId" INTEGER REFERENCES activities(id),
+  duration INTEGER,
+  count INTEGER
+  UNIQUE("routineId", "activityId")
+);
+`)
 }
 
 /* 
