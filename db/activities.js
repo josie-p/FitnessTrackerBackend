58ae95f6a6fd84255a3a811069f80a7402e577f6
Your Name <parker.josie.eden@gmail.com>
2023-03-13 13:40:00 -0500

    getAllActivities, updateActivities, and getActivityByName are all passsing tests :)

--- db/activities.js
+++ db/activities.js
@@ -18,6 +18,16 @@ async function createActivity({ name, description }) {
 
 async function getAllActivities() {
   // select and return an array of all activities
+  try{
+    const { rows } = await client.query(`
+    SELECT *
+    FROM activities;
+    ` );
+
+    return rows;
+  }catch(error){
+    throw error;
+  }
 }
 
 async function getActivityById(id) {
@@ -34,7 +44,19 @@ async function getActivityById(id) {
 }
 
 
-async function getActivityByName(name) {}
+async function getActivityByName(name) {
+  try{
+    const { rows: [activity] } = await client.query(`
+    SELECT *
+    FROM activities
+    WHERE name = $1;
+    `, [name]);
+
+    return activity;
+  }catch(error){
+    throw error;
+  }
+}
 
 // used as a helper inside db/routines.js
 async function attachActivitiesToRoutines(routines) {     
@@ -76,6 +98,28 @@ async function updateActivity({ id, ...fields }) {
   // don't try to update the id
   // do update the name and description
   // return the updated activity
+
+  const setString = Object.keys(fields).map((key, index) => `"${key}"=$${index + 1}`)
+  .join(" ");
+
+  if(setString.length === 0){
+    return;
+  }
+
+  try{
+   const { rows: [activity] } = await client.query(`
+   UPDATE activities
+   SET ${setString}
+   WHERE id = ${id}
+   RETURNING *;
+   `, Object.values(fields));
+
+   return activity;
+
+  }catch(error){
+    throw error;
+  }
+
 }
 
 module.exports = {
