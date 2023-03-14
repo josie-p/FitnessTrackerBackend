85e86210f4fe5bdd1ff557b63d74e2f4973ad359
Your Name <parker.josie.eden@gmail.com>
2023-03-14 10:59:21 -0500

    continued working on getAllRoutines

--- db/routines.js
+++ db/routines.js
@@ -46,25 +46,67 @@ async function getRoutinesWithoutActivities() {
 // "creatorId", "isPublic", name, duration, count, "routineId", "routineActivityId", username AS "creatorName"
 async function getAllRoutines() {
   try{
-    const { rows: routine } = await client.query(`
-      SELECT *
-      FROM routines;
-    `);
-    const { rows: [activities] } = await client.query(`
-      SELECT routine_activities.*
-      FROM routine_activities
-      JOIN routines
-      ON routines.id = "routineId";
-    `)
-    const { rows: [creatorName] } = await client.query(`
-      SELECT username AS "creatorName"
-      FROM users;
-    `)
+    // const { rows: routine } = await client.query(`
+    //   SELECT *
+    //   FROM routines;
+    // `);
+    // const { rows: [activities] } = await client.query(`
+    //   SELECT routine_activities.*
+    //   FROM routine_activities
+    //   JOIN routines
+    //   ON routines.id = "routineId";
+    // `)
+    // const { rows: [creatorName] } = await client.query(`
+    //   SELECT username AS "creatorName"
+    //   FROM users;
+    // `)
     
-    routine.activities = activities;
-    routine.creatorName = creatorName;
-    console.log(routine, "this is routine");
-    return routine
+    // routine.activities = activities;
+    // routine.creatorName = creatorName;
+    // console.log(routine, "this is routine");
+    // return routine
+
+    const { rows : routines } = await client.query(`
+    SELECT *
+    FROM routines;
+    `)
+
+    const { rows : routine_activities } = await client.query(`
+    SELECT *
+    FROM routine_activities;
+    `)
+
+    const { rows : users } = await client.query(`
+    SELECT *
+    FROM users;
+    `)
+
+    const { rows: activities } = await client.query(`
+    SELECT *
+    FROM activities;
+    `)
+
+    // console.log( "routines: ", routines);
+    // console.log("routine_activities: ", routine_activities);
+    // console.log("activities: ", activities);
+    // console.log("users: ", users);
+
+    const { rows: routinesjoin } = await client.query(`
+    SELECT routines.*, routine_activities.*, username as "creatorName", activities.name AS activityname
+    FROM routines JOIN routine_activities ON routines.id = "routineId"
+    JOIN users ON routines."creatorId" = users.id
+    JOIN activities ON activities.id = routine_activities."activityId";
+    `);
+
+    const { rows: userjoin } = await client.query(`
+    SELECT username AS "creatorName"
+    FROM users JOIN routines ON routines."creatorId" = users.id;
+    `)
+
+    // console.log("result from join: ", routinesjoin);
+    // console.log("result from usersjoin: ", userjoin);
+
+    return routinesjoin;
   } catch(error){
     throw error;
   }
