14dda60fd5491e9b4fc5ad9a98dcc53852402de4
Your Name <parker.josie.eden@gmail.com>
2023-03-15 10:52:15 -0500

    started working on routes, something wrong with server

--- api/users.js
+++ api/users.js
@@ -1,8 +1,41 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { createUser, getUser, getUserByUsername } = require("../db");
 
 // POST /api/users/register
+router.post("/register", async(req, res, next) => {
+    const { username, password } = req.body;
+
+    try{
+        const _user = await getUserByUsername(username);
+
+        const token =jwt.sign(
+            {
+                username: user.username,
+                id: user.id,
+            },
+            process.env.JWT_SECRET, {
+                expiresIn: "1w"
+            }
+        );
+
+        if(_user){
+            next({
+                name: "UserExistsError",
+                message: "A user by that name already exists"
+            });
+        }
+
+        const user = await createUser({ username, password });
+
+        res.send({ message: "thank you for signing up!", token });
+
+    }catch({ name, message }){
+        next({ name, message });
+    }
+})
 
 // POST /api/users/login
 
--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 require("dotenv").config()
-const express = require("express")
+const express = require("express");
+const morgan = require("morgan");
 const app = express()
+const cors = require("cors");
 
 // Setup your Middleware and API Router here
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    console.log("---- Body Logger Start ----");
+    console.log(req.body);
+    console.log("---- Body Logger End ----");
+
+    next();
+})
+
+const apiRouter = require("./api");
+app.use("./api", apiRouter);
+
+const client  = require("./db/client");
+client.connect();
 
 module.exports = app;
