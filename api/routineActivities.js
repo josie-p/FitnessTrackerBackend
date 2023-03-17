83ffed9a4346d9d612ed77285875868766be1cf8
Alexandra <alexmiriamd@gmail.com>
2023-03-17 09:39:51 -0600

    updateRoutineActivity route passing all tests

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,7 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const { requireUser } = require('./utils');
+
+const {
+    updateRoutineActivity,
+    canEditRoutineActivity,
+    getRoutineById,
+    getRoutineActivityById
+} = require("../db");
+
 
 // PATCH /api/routine_activities/:routineActivityId
+router.patch("/:routineActivityId", requireUser, async (req, res, next) => {
+    const  { routineActivityId } = req.params;
+    const { count, duration } = req.body;
+    const { id, username } = req.user;
+
+    const fields = {};
+
+    if(count){
+        fields.count = count;
+    }
+    if(duration){
+        fields.duration = duration;
+    }
+
+    const findRoutineActivityForName = await getRoutineActivityById(routineActivityId); //getting name of routine for error message
+    const findName = await getRoutineById(findRoutineActivityForName.routineId);
+    const name = findName.name;
+   
+    try{
+        const findRoutineActivity = await canEditRoutineActivity(routineActivityId, id);
+      
+        if(!findRoutineActivity){
+            next({
+                name: "UnauthorizedUserError",
+                message: `User ${username} is not allowed to update ${name}`,
+                error: "Not Creator of Routine_Activity"
+            })
+        }
+
+        const updatedRoutineActivity = await updateRoutineActivity({id: routineActivityId, ...fields});
+        res.send(updatedRoutineActivity);
+    } catch({ name, error, message}){
+        next({name, error, message});
+    }
+})
 
 // DELETE /api/routine_activities/:routineActivityId
 
